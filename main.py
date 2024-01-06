ca8ca9e5a76249dd15b5614cccf34e95b1d5bde8
tarasbmal <tarasbmal@gmail.com>
2024-01-06 19:19:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -141,12 +141,13 @@ if st.session_state['button'] == True:
                 f_enddate = datetime.datetime.strptime(fi_mat, "%d-%m-%Y")
             f_endsum = float(fi_nominal)    
             #---------   Добавляем налог с купонов ---
-            #if use_nalog:
+            if use_nalog:
                 #for index,row in pl.iterrows():
                 #        pl.loc[len(pl.index)] = [pl['dates'][index],round(pl['amounts'][index]*(-0.13),2)]
                 #--- если купили дешевле, то еще в конце срока
-                #if f_buysum < f_endsum:
-                #    pl.loc[len(pl.index)] = [f_enddate,round((f_endsum-f_buysum)*(-0.13),2)]                              
+                if f_buysum < f_endsum:
+                #    pl.loc[len(pl.index)] = [f_enddate,round((f_endsum-f_buysum)*(-0.13),2)]      
+                    pl.loc[len(pl.index)] = [f_enddate,123]                      
             #---------   Добавляем покупку  -----------
             pl.loc[len(pl.index)] = [f_buydate, f_buysum*(-1)]
             #---------   Добавляем погашение -----
